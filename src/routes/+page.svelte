a4d1853f33d914e53af49ef057798d3f3d3088b5
Danilo Matrangolo Marano <danilo.marano@mac.com.br>
2023-07-21 16:03:14 -0300

    Configured v3.3.3 Tailwind's installation recomendation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
+    let count = 0
+    
+    function incrementCounter() {
+        count += 1
+    }
 </script>
 
 <svelte:head>
@@ -9,51 +11,13 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
+<section class='flex justify-center items-center w-full h-full gap-4 bg-slate-900 text-white'>
+    <p class='font-medium text-lg'>Counter app: </p>
+    <button
+        class='rounded bg-indigo-500 px-4 py-2'
+        on:click={incrementCounter}
+    >
+        {count}
+    </button>
 </section>
 
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
