71f21df66ed0aa231edc7379d7225a3869eb9080
Danilo Marano <danilo.m.marano@gmail.com>
2023-08-26 15:06:54 -0300

    Page to see badges variations

--- src/routes/badges/+page.svelte
+++ src/routes/badges/+page.svelte
@@ -1,6 +1,7 @@
 <script>
     import Badge from '../../../components/Badge.svelte'
-    import Icons from '../../../components/Icons.svelte'
+    import MachineLearningModel from '../../../components/icons/MachineLearningModel.svelte';
+    import Gamification from '../../../components/icons/Gamification.svelte';
 </script>
 
 <svelte:head>
@@ -8,134 +9,75 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<div class='grid grid-cols-5 gap-4 w-fit'>
-    <Badge color='blue'>
-        <Icons class='h-4 w-4 text-blue-500' />
-        <p >Teste</p>
-    </Badge>
-    <Badge color='blue'>
-        <p >Teste</p>
-    </Badge>
-    <Badge color='blue'>
-        <p >Teste</p>
-    </Badge>
-    <Badge color='blue'>
-        <p >Teste</p>
-    </Badge>
-    <Badge color='blue'>
-        <p >Teste</p>
-    </Badge>
+<div class='flex flex-col justify-center items-center h-screen'>
+    <div class='flex flex-col gap-8'>
+        <p class='text-6xl font-bold text-gray-600'>Badges</p>
+        <div class='grid grid-cols-6 justify-center items-center gap-8 w-fit'>
+            <Badge icon={MachineLearningModel} color='lime'>
+                <p >Machine Learning</p>
+            </Badge>
 
-    <Badge color='red'>
-        <p >Teste</p>
-    </Badge>
-    <Badge color='red'>
-        <p >Teste</p>
-    </Badge>
-    <Badge color='red'>
-        <p >Teste</p>
-    </Badge>
-    <Badge color='red'>
-        <p >Teste</p>
-    </Badge>
-    <Badge color='red'>
-        <p >Teste</p>
-    </Badge>
+            <Badge icon={Gamification} color='orange'>
+                <p >Machine Learning</p>
+            </Badge>
 
-    <Badge color='green'>
-        <p >Teste</p>
-    </Badge>
-    <Badge color='green'>
-        <p >Teste</p>
-    </Badge>
-    <Badge color='green'>
-        <p >Teste</p>
-    </Badge>
-    <Badge color='green'>
-        <p >Teste</p>
-    </Badge>
-    <Badge color='green'>
-        <p >Teste</p>
-    </Badge>
+            <Badge icon={MachineLearningModel} color='pink'>
+                <p >Machine Learning</p>
+            </Badge>
 
-    <Badge color='yellow'>
-        <p >Teste</p>
-    </Badge>
-    <Badge color='yellow'>
-        <p >Teste</p>
-    </Badge>
-    <Badge color='yellow'>
-        <p >Teste</p>
-    </Badge>
-    <Badge color='yellow'>
-        <p >Teste</p>
-    </Badge>
-    <Badge color='yellow'>
-        <p >Teste</p>
-    </Badge>
+            <Badge icon={Gamification} color='purple'>
+                <p >Machine Learning</p>
+            </Badge>
 
-    <Badge color='indigo'>
-        <p >Teste</p>
-    </Badge>
-    <Badge color='indigo'>
-        <p >Teste</p>
-    </Badge>
-    <Badge color='indigo'>
-        <p >Teste</p>
-    </Badge>
-    <Badge color='indigo'>
-        <p >Teste</p>
-    </Badge>
-    <Badge color='indigo'>
-        <p >Teste</p>
-    </Badge>
+            <Badge icon={MachineLearningModel} color='red'>
+                <p >Machine Learning</p>
+            </Badge>
 
-    <Badge color='gray'>
-        <p >Teste</p>
-    </Badge>
-    <Badge color='gray'>
-        <p >Teste</p>
-    </Badge>
-    <Badge color='gray'>
-        <p >Teste</p>
-    </Badge>
-    <Badge color='gray'>
-        <p >Teste</p>
-    </Badge>
-    <Badge color='gray'>
-        <p >Teste</p>
-    </Badge>
+            <Badge icon={Gamification} color='sky'>
+                <p >Machine Learning</p>
+            </Badge>
 
-    <Badge color='white'>
-        <p >Teste</p>
-    </Badge>
-    <Badge color='white'>
-        <p >Teste</p>
-    </Badge>
-    <Badge color='white'>
-        <p >Teste</p>
-    </Badge>
-    <Badge color='white'>
-        <p >Teste</p>
-    </Badge>
-    <Badge color='white'>
-        <p >Teste</p>
-    </Badge>
+            <Badge icon={MachineLearningModel} color='slate'>
+                <p >Machine Learning</p>
+            </Badge>
 
-    <Badge color='black'>
-        <p >Teste</p>
-    </Badge>
-    <Badge color='black'>
-        <p >Teste</p>
-    </Badge>
-    <Badge color='black'>
-        <p >Teste</p>
-    </Badge>
-    <Badge color='black'>
-        <p >Teste</p>
-    </Badge>
-    <Badge color='black'>
-        <p >Teste</p>
-    </Badge>
+            <Badge icon={Gamification} color='teal'>
+                <p >Machine Learning</p>
+            </Badge>
+
+            <Badge icon={MachineLearningModel} color='violet'>
+                <p >Machine Learning</p>
+            </Badge>
+
+            <Badge icon={MachineLearningModel} color='amber'>
+                <p >Machine Learning</p>
+            </Badge>
+
+            <Badge icon={Gamification} color='blue'>
+                <p >Machine Learning</p>
+            </Badge>
+
+            <Badge icon={MachineLearningModel} color='cyan'>
+                <p >Machine Learning</p>
+            </Badge>
+
+            <Badge icon={Gamification} color='emerald'>
+                <p >Machine Learning</p>
+            </Badge>
+
+            <Badge icon={MachineLearningModel} color='fuchsia'>
+                <p >Machine Learning</p>
+            </Badge>
+
+            <Badge icon={Gamification} color='green'>
+                <p >Machine Learning</p>
+            </Badge>
+
+            <Badge icon={MachineLearningModel} color='indigo'>
+                <p >Machine Learning</p>
+            </Badge>
+
+        </div>
+    </div>
 
 </div>
